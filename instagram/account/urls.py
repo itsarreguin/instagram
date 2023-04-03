8d65ea6cc9d7e8a9a42701be23e6b14e9e1dc33b
Brandon Arreguin <arreguin.tech@gmail.com>
2023-04-02 22:11:42 -0500

    First apps created, 'account', 'core'

---
+++ instagram/account/urls.py
@@ -0,0 +1,8 @@
+# Python stantad library
+from typing import List
+
+# Django imports
+from django.urls import path
+
+
+urlpatterns: List[path] = []
--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,4 @@
-"""instagram URL Configuration
+"""Instagram URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,14 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# Python standard library
+from typing import List
 
+# Django imports
 from django.urls import path
 from django.urls import include
 from django.contrib import admin
 from django.conf import settings
 
 
-urlpatterns = []
+urlpatterns: List[path] = []
 
 if settings.DEBUG:
     urlpatterns += [path('admin/', admin.site.urls)]
