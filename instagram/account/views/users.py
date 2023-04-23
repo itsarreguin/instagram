729c9a2f67abe67fdcf636cc1a636de1fc9718f7
Brandon Arreguin <arreguin.tech@gmail.com>
2023-04-22 19:47:55 -0600

    Post images displaying by profile user

--- instagram/account/views/users.py
+++ instagram/account/views/users.py
@@ -17,7 +17,7 @@ from django.contrib.auth.views import PasswordChangeView
 # Django DB
 from django.db.models import QuerySet
 from django.db.models import Model
-# Django auth and shortcuts
+# Django contrib and shortcuts
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
@@ -30,49 +30,12 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 # Instagram models
-from instagram.account.models import User
 from instagram.account.models import Profile
 from instagram.posts.models import Post
 # Instagram forms
 from instagram.account.forms.user import EditProfileForm
 from instagram.account.forms.user import EditAccountForm
 from instagram.account.forms.user import ChangePasswordForm
-
-
-posts: list[dict[str, str | int]] = [
-    {
-        'id': 1,
-        'username': 'ronacher',
-        'picture': 'https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-        'first_name': 'Armin',
-        'last_name': 'Ronacher',
-        'image': 'https://images.unsplash.com/photo-1680553437316-d80361108647?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2089&q=80'
-    },
-    {
-        'id': 2,
-        'username': 'yisuscraist',
-        'picture': 'https://images.theconversation.com/files/346421/original/file-20200708-3995-5ulgxa.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip',
-        'first_name': 'Jesus',
-        'last_name': 'de Nazaret',
-        'image': 'https://images.unsplash.com/photo-1672745256937-4e60a4534413?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=788&q=80'
-    },
-    {
-        'id': 3,
-        'username': 'chuck',
-        'picture': 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-        'first_name': 'Chuck',
-        'last_name': 'Berry',
-        'image': 'https://images.unsplash.com/photo-1680585934031-8842e4cf82c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-    },
-    {
-        'id': 4,
-        'username': 'ronacher',
-        'picture': 'https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-        'first_name': 'Armin',
-        'last_name': 'Ronacher',
-        'image': 'https://images.unsplash.com/photo-1680466257600-86e0aa02cc64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-    },
-]
 
 
 class FeedView(LoginRequiredMixin, TemplateView):
@@ -100,6 +63,7 @@ class ProfileView(LoginRequiredMixin, ContextMixin, View):
     def get_context_data(self, **kwargs: Dict[str, Any]) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['user'] = self.get_queryset(username=self.kwargs['username'])
+        context['posts'] = Post.objects.filter(author__username=self.kwargs['username']).all()
         
         return context
     
