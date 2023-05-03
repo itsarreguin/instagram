fdf92f68b5ef8fffd71ff2941ff8d3d4ce358795
Brandon Arreguin <arreguin.tech@gmail.com>
2023-05-02 19:40:19 -0600

    Deleting notifications

--- instagram/notifications/urls.py
+++ instagram/notifications/urls.py
@@ -6,6 +6,7 @@ from django.urls import path
 
 # Instagram views
 from instagram.notifications.views import NotificationsView
+from instagram.notifications.views import NotificationDeleteView
 
 
 app_name: str = 'notifications'
@@ -15,5 +16,10 @@ urlpatterns: List[path] = [
         route='notifications/',
         view=NotificationsView.as_view(),
         name='all'
+    ),
+    path(
+        route='notifications/<int:pk>/delete',
+        view=NotificationDeleteView.as_view(),
+        name='delete'
     ),
 ]
--- instagram/notifications/views.py
+++ instagram/notifications/views.py
@@ -1,13 +1,24 @@
 # Python standard library
 from typing import Any
 from typing import Dict
+from typing import Tuple
 
+# Django HTTP package
+from django.http import HttpRequest
+from django.http import HttpResponse
 # Django views
+from django.views import View
 from django.views.generic import TemplateView
 # Django contrib
 from django.contrib.auth.mixins import LoginRequiredMixin
+# Django database
+from django.db.models import QuerySet
 # Django utils
 from django.utils.translation import gettext_lazy as _
+
+# Instagram models
+from instagram.notifications.models import Notification
+from instagram.notifications.models import NotificationType
 
 
 class NotificationsView(LoginRequiredMixin, TemplateView):
@@ -20,3 +31,16 @@ class NotificationsView(LoginRequiredMixin, TemplateView):
         context['title'] = self.template_title
         
         return context
+
+
+class NotificationDeleteView(LoginRequiredMixin, View):
+    
+    def get_queryset(self, *args: Tuple[Any], **kwargs: Dict[str, Any]) -> QuerySet:
+        return Notification.objects.filter(*args, **kwargs)
+    
+    def delete(self, request: HttpRequest, **kwargs: Dict[str, Any]) -> HttpResponse:
+        notification = self.get_queryset(pk=kwargs['pk']).first()
+        if notification:
+            notification.delete()
+        
+        return HttpResponse()
